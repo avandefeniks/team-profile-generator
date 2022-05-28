00520d44784f0ab1993d3be67fb9326a013ad5e0
avandefeniks <angel.vandefeniks@gmail.com>
2022-05-27 19:41:18 -0700

    completed manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,6 +3,14 @@ const Manager = require('../lib/Manager');
 test('Test to check if properties have been set up', () => {
     const manager = new Manager(1);
 
-    expect(manager.officeNumber).toEqual(1);
-    expect(manager.role).toEqual('Manager');
+    expect(manager.officeNumber).toBe(1);
+    expect(manager.role).toBe('Manager');
+});
+
+test('test to make sure getRole is working', () => {
+    const manager = new Manager(1);
+
+    let man = manager.getRole('Manager')
+
+    expect(man).toEqual('Manager');
 })
